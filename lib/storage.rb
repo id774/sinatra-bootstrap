4d39885a26fb5475261c263a0050682b9899804a
id774 <idnanashi@gmail.com>
2016-06-07 09:56:23 +0900

    33 Refactoring Storage class

--- lib/storage.rb
+++ lib/storage.rb
@@ -6,6 +6,15 @@ require 'will_paginate'
 require 'will_paginate/active_record'
 
 class Content < ActiveRecord::Base
+  def self.columns_definition
+    {
+      :key => :string,
+      :value => :string,
+      :created_at => :datetime,
+      :updated_at => :datetime
+    }
+  end
+
   self.per_page = 5
 end
 
@@ -15,11 +24,11 @@ class Storage
   end
 
   def create
-    create_table unless model_class.table_exists?
+    create_table(model_class)
   end
 
   def drop
-    drop_table if model_class.table_exists?
+    drop_table(model_class)
   end
 
   private
@@ -28,35 +37,22 @@ class Storage
     ActiveRecord::Base.configurations = YAML.load_file(File.expand_path(File.join(File.dirname(__FILE__),
       '..', 'config', 'database.yml')))
     ActiveRecord::Base.establish_connection(:development)
-    create_table unless model_class.table_exists?
+    create
   end
 
   def model_class
     Content
   end
 
-  def column_definition
-    {
-      :key => :string,
-      :value => :string,
-      :created_at => :datetime,
-      :updated_at => :datetime
-    }
-  end
-
-  def unique_key
-    :id
-  end
-
-  def create_table
-    ActiveRecord::Migration.create_table(model_class.table_name){|t|
-      column_definition.each_pair {|column_name, column_type|
+  def create_table(target_class)
+    ActiveRecord::Migration.create_table(target_class.table_name){|t|
+      target_class.columns_definition.each_pair {|column_name, column_type|
         t.column column_name, column_type
       }
-    }
+    } unless target_class.table_exists?
   end
 
-  def drop_table
-    ActiveRecord::Migration.drop_table(model_class.table_name)
+  def drop_table(target_class)
+    ActiveRecord::Migration.drop_table(target_class.table_name) unless target_class.table_exists?
   end
 end
