b230043b3139ba8e540d760631a2065fc4adb3c2
id774 <idnanashi@gmail.com>
2016-06-09 15:17:32 +0900

    41 Refactoring test

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,18 +1,18 @@
 # coding: utf-8
 
-ENV['RACK_ENV'] = "test"
-
-require File.dirname(__FILE__) + '/../app'
-require File.dirname(__FILE__) + '/../lib/storage'
-require File.dirname(__FILE__) + '/../lib/paginate'
-
 require 'rspec'
 require 'rack/test'
+
+ENV['RACK_ENV'] = "test"
+
+require File.join(File.dirname(__FILE__), '..', 'app.rb')
+require File.join(File.dirname(__FILE__), '..', 'lib', 'storage.rb')
+require File.join(File.dirname(__FILE__), '..', 'lib', 'paginate.rb')
 
 RSpec.configure do |conf|
   conf.include Rack::Test::Methods
 end
 
 def app
-  SinatraBootstrap
+  Sinatra::Application
 end
